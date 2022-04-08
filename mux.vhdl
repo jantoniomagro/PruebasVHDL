a0cb4598d7a7f357e4b040952db0fe9e085fcc67
jantoniomagro <jantonio.magro@gmail.com>
2022-04-08 05:15:20 +0200

    más código

--- mux.vhdl
+++ mux.vhdl
@@ -10,7 +10,39 @@ ENTITY mux IS
     );
 END mux;
 
+-- arquitectura
+ARCHITECTURE comportam1 of mux is
+BEGIN
+
+    p_mux : PROCESS (p0, p1, sel) -- lista sensibilidad (señales leídas)
+    BEGIN
+        IF sel = '0' THEN
+            ps <= p0;
+        ELSE
+            ps <= p1;
+        END IF;
+    END PROCESS;
+
+END ARCHITECTURE comportam1;
+
+-- arquitectura
+ARCHITECTURE comportam0 OF mux IS
+BEGIN
+    ps <= p0 WHEN sel = '0' ELSE
+        p1;
+END ARCHITECTURE comportam0;
+
+--arquitectura
 ARCHITECTURE gate1 OF mux IS
 BEGIN
     ps <= (p0 AND (NOT sel)) OR (p1 AND sel);
 END ARCHITECTURE gate1;
+
+-- arquitectura
+ARCHITECTURE gate2 OF mux IS
+    SIGNAL s0, s1 : STD_LOGIC;
+BEGIN
+    s0 <= (p0 AND (NOT sel));
+    s1 <= p1 AND sel;
+    ps <= s0 OR s1;
+END ARCHITECTURE gate2;
